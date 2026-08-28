04ea878bf9052116e66b894719df277d79106e01
June <june@Junes-MacBook-Pro.local>
2026-08-28 21:20:41 +0900

    7ㅂㅓㄴㅁㅜㄴㅈㅔ ㅎㅐㄱㅕㄹ

--- week2/1. basic/07_complexity.py
+++ week2/1. basic/07_complexity.py
@@ -35,6 +35,14 @@ def find_duplicates_brute_force(nums):
     ## i번째 원소와 i+1 이후의 모든 원소를 비교
     ## 같은 원소를 찾으면 duplicates에 추가 (중복 추가 방지 필요)
     pass
+
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] == nums[j]:
+                if nums[i] not in duplicates:
+                    duplicates.append(nums[i])
+                else:
+                    break            
     
     return duplicates
 
@@ -42,7 +50,7 @@ def find_duplicates_sorting(nums):
     """
     방법2: 정렬 후 인접 원소 비교
     시간 복잡도: O(n log n) - 정렬
-    공간 복잡도: O(1) - 정렬을 in-place로 수행
+    공간 복잡도: O(1) - 정렬을 in-place로 수행 -> in-place: 새로운 것을 반환하는것이 아닌 자신을 바꾸는것
     """
     if not nums:
         return []
@@ -55,6 +63,17 @@ def find_duplicates_sorting(nums):
     # TODO: 인접한 원소를 비교하여 중복 찾기
     # i와 i+1 원소가 같고, duplicates에 없으면 추가
     pass
+
+    nums.sort()
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1]:
+            num = nums[i]
+            if num not in duplicates:
+                duplicates.append(num)
+            else:
+                continue
+        else:
+            continue
     
     return duplicates
 
@@ -71,7 +90,15 @@ def find_duplicates_hash(nums):
     ## 이미 seen에 있으면 duplicates에 추가
     ## 없으면 seen에 추가
     pass
+
     
+        #중복없이 정리됨
+        
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
     return list(duplicates)
 
 def measure_time(func, nums, method_name):
